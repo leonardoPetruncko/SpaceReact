e15f196a38110c760e60b6fb8ce8f82ea19a7dc5
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-01-09 16:17:41 -0300

    Add Componente menu e banner

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,21 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import banner from './banner.png'
+import styles from "./PaginaInicial.module.scss"
 
 export default function PaginaInicial() {
     return (
+        <>
         <Cabecalho />
+        <main>
+            <section className={styles.principal}>
+            <Menu />
+            <div className={styles.principal__imagem}>
+                <h1>A galeria mais completa do Espaço</h1>
+                <img src={banner} alt="A imagem da terra vista do espaço" />
+            </div>
+            </section>
+        </main>
+        </>
     )
 }
