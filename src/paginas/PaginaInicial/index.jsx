643875de64d1fb6f356ebf26512713cb8f7dc16f
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-01-09 17:02:12 -0300

    Componente Rodape

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,5 +1,6 @@
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import Rodape from "../../componentes/Rodape";
 import banner from './banner.png'
 import styles from "./PaginaInicial.module.scss"
 
@@ -16,6 +17,7 @@ export default function PaginaInicial() {
             </div>
             </section>
         </main>
+        <Rodape />
         </>
     )
 }
