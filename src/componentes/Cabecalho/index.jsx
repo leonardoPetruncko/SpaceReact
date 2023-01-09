544c941930b7563293847c75fd9dbe3fb514a736
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-01-09 15:20:04 -0300

    Estilizando a pag

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,12 +1,13 @@
 import logo from './logo.png'
 import search from './search.png'
+import styles from './Cabecalho.module.scss'
 
 export default function Cabecalho() {
     return (
-        <header>
+        <header className={styles.cabecalho}>
             <img src={logo} alt="Logo do Space" />
-            <div>
-                <input type="text"
+            <div className={styles.cabecalho__container}>
+                <input className={styles.cabecalho__input} type="text"
                 placeholder="O que Deseja?" />
                 <img src={search} alt="icone da lupa"/>
             </div>
