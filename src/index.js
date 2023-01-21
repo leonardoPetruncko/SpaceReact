b2ad8f5eadbba4a3d073f16b950c9ec8c1b4e064
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-01-20 23:54:18 -0300

    d

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./assets/fontes/GandhiSans-Bold.otf"
 
 import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <App />
